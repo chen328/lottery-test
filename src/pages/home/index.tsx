7707d01977d4fbf18879bfa1836856bda8a64848
飞艳 宋 <feiyan.s@shulidata.com>
2024-07-25 14:24:06 +0800

    feat: 新增首页

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,32 +1,87 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Swiper, Image } from 'antd-mobile';
 import { useQuery } from '@/public/hooks';
-import { trackEnterPage } from '@/public/track/index';
-import { homePrizesList } from '@/public/service/home';
+import { homePrizesList, queryRecentLuckDogs as queryRecentLuckDogsApi } from '@/public/service/home';
+import Loading from '@/components/Loading';
 import { BUILTIN_CHANNELS } from './config';
+import PrizeItem from './components/PrizeItem';
 import './index.less';
 
 function Home() {
 	const { source } = useQuery();
+	const [luckDogs, setLuckDogs] = useState<any[]>([{}]);
+	const [campInfoVoList, setCampInfoVoList] = useState<any[]>([]);
+	const [loading, setLoading] = useState(false);
 
-	useEffect(() => {
-		trackEnterPage({ pageId: source });
-		
-		getList();
-	}, []);
-
-	const getList = () => {
+	const getList = (page = 1) => {
+		setLoading(true);
 		homePrizesList({
-			page: 1,
+			page,
 			pageSize: 50,
 			displayChannel: 'LOTTERY_LIST',
 			displayColumn: 'NORMAL',
 			regionId: BUILTIN_CHANNELS.official.id,
-		}).then((data) => {
-			console.log(data);
+			displaySubChannel: 'H5',
+		}).then((res) => {
+			setCampInfoVoList(res.campInfoVoList || []);
+		}).finally(() => {
+			setLoading(false);
 		});
 	};
 
-	return <div>首页</div>;
+	const queryRecentLuckDogs = async () => {
+		const res = await queryRecentLuckDogsApi({
+			page: 1,
+			pageSize: 20,
+		});
+		setLuckDogs(res.luckDogs || []);
+	};
+
+	useEffect(() => {
+		// trackEnterPage({ pageId: source });
+		getList();
+		queryRecentLuckDogs();
+	}, []);
+
+	return (
+		<div className='home'>
+			{loading ? (
+				<Loading text='加载中' style={{ fontSize: '12px' }} />
+			) : (
+				<>
+					{/* 中奖轮播放 */}
+					{luckDogs?.length > 0 ? (
+						<div className='home__carousel text-ellipsis'>
+							<Swiper autoplay direction='vertical' indicator={() => null} loop style={{ '--height': '62px' }}>
+								{luckDogs.map((item, index) => (
+									<Swiper.Item key={index}>
+										<div className='text-ellipsis '>
+											恭喜{item.userName || '匿名'}中奖
+											{item.lotteryPrizeInfoVoList?.map((prizeItem: any, index) => (
+												<div key={index}>{index > 0 ? '，' + prizeItem.subject : prizeItem.subject}</div>
+											))}
+										</div>
+									</Swiper.Item>
+								))}
+							</Swiper>
+						</div>
+					) : null}
+
+					<div className='home__prize-list'>
+						{campInfoVoList.length > 0 ? (
+							<>
+								{campInfoVoList.map(item => (
+									<PrizeItem key={item.campId} item={item} itemList={campInfoVoList} />
+								))}
+							</>
+						) : (
+							<Image className='home__empty-img' src='https://sl-online-oss.shulidata.com/resource/lottery_everyday/v2_71_0/empty.png' />
+						)}
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default Home;
--- src/public/service/home.ts
+++ src/public/service/home.ts
@@ -16,3 +16,20 @@ export function homePrizesList(queryParams) {
 		},
 	});
 }
+
+/**
+ * @name C端/聚合页页面-查询最近中奖信息
+ * @api http://sapi.shuli.com/project/187/interface/api/33276
+ */
+export function queryRecentLuckDogs(queryParams) {
+	return request({
+		method: 'POST',
+		url: '/ipsponsorprod/lottery/camp/recent/luck/dog/list',
+		headers: {
+			'content-type': 'application/json',
+		},
+		data: {
+			...queryParams,
+		},
+	});
+}
