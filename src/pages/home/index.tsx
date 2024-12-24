085e17c3dbb4a1af645952debb07b51edaf0e2f1
chen328 <820960276@qq.com>
2024-12-24 09:53:34 +0800

    test

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -17,7 +17,7 @@ function Home() {
 	const [luckDogs, setLuckDogs] = useState<any[]>([{}]);
 	const [campInfoVoList, setCampInfoVoList] = useState<any[]>([]);
 	const [loading, setLoading] = useState(false);
-	const b;
+	const c;
 	const getList = (page = 1, isFirst = false) => {
 		setLoading(true);
 		homePrizesList({
